21f53c461807bad624fe2d332efcede4c7031521
kostya-lee003 <kostyalee003@gmail.com>
2023-02-03 06:16:37 +0500

    first commit

---
+++ CoreMLBackgroundChangeSwiftUI/CoreMLBackgroundChangeSwiftUI/CoreMLBackgroundChangeSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreMLBackgroundChangeSwiftUIApp.swift
+//  CoreMLBackgroundChangeSwiftUI
+//
+//  Created by Kostya Lee on 02/02/23.
+//
+
+import SwiftUI
+
+@main
+struct CoreMLBackgroundChangeSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
